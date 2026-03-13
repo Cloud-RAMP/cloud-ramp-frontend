28ac0378bd5367ad7f8a25ecf366973fa009ba66
PeterOlsen1 <peterolsen455@gmail.com>
2026-03-12 22:24:54 -0500

    feat: add square bg

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Heading from "@/components/text/Heading";
 
 export default function Home() {
     return (
-        <VStack className="min-h-screen font-sans dark:bg-black justify-center" gap="gap-32">
+        <VStack className="min-h-screen font-sans dark:bg-black justify-center square-bg" gap="gap-32">
             <VStack>
                 <Heading>Build WebSocket apps, scale fast, remove overhead</Heading>
                 <Heading className="font-bold">CloudRamp</Heading>
