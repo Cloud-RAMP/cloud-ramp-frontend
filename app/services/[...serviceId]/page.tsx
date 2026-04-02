dae470f06776dc6463ed68d4906fe38134b68d3f
PeterOlsen1 <peterolsen455@gmail.com>
2026-04-02 17:00:29 -0500

    feat: add firebase live update logs function

--- app/services/[...serviceId]/page.tsx
+++ app/services/[...serviceId]/page.tsx
@@ -3,10 +3,23 @@
 import PageContainer from '@/components/layout/PageContainer';
 import { useParams } from 'next/navigation';
 import Heading from '@/components/text/Heading';
+import { useEffect, useState } from 'react';
+import { subscribeToServiceLogs } from '@/firebase/firestore';
 
 export default function ServiceIdPage() {
   const params = useParams();
-  const serviceId = params.serviceId;
+  const serviceIdParam = params.serviceId;
+  const serviceId = Array.isArray(serviceIdParam) ? serviceIdParam[0] : String(serviceIdParam)
+
+  const [logs, setLogs] = useState<string[]>([]);
+
+  useEffect(() => {
+    const unsub = subscribeToServiceLogs(serviceId, (logs) => {
+      console.log(logs);
+    });
+
+    return unsub;
+  }, []);
 
   return (
     <PageContainer>
